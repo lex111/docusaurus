0232b301e2a9d472221a46076133914413c0281e
Alexey Pyltsyn <lex61rus@gmail.com>
2019-11-14 17:06:52 +0300

    add dynamically change Prism theme

--- packages/docusaurus-theme-classic/src/theme/Navbar/index.js
+++ packages/docusaurus-theme-classic/src/theme/Navbar/index.js
@@ -12,6 +12,7 @@ import Link from '@docusaurus/Link';
 import Head from '@docusaurus/Head';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import {dispatch} from '@docusaurus/useEventBus';
 
 import SearchBar from '@theme/SearchBar';
 
@@ -63,7 +64,11 @@ function Navbar() {
   }, [setSidebarShown]);
 
   const onToggleChange = useCallback(
-    e => setTheme(e.target.checked ? 'dark' : ''),
+    e => {
+      const newTheme = e.target.checked ? 'dark' : '';
+      setTheme(newTheme);
+      dispatch('docusaurus-change-theme', {newTheme});
+    },
     [setTheme],
   );
 
